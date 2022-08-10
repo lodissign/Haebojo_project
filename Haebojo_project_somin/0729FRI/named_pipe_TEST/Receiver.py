27daf2ae69e762e1e0aedcceff5275a4db11ab71
lodissign <105443266+lodissign@users.noreply.github.com>
2022-08-10 12:38:47 +0900

    Delete Haebojo_project_somin directory

--- Haebojo_project_somin/0729FRI/named_pipe_TEST/Receiver.py
+++
@@ -1,7 +0,0 @@
-import os
-import time
-import errno
-
-def Receiver():
-    path="pipe"
-    pipe=os.open(path,os.O_RDONLD | )
--- Haebojo_project_somin/README.md
+++
@@ -1,6 +0,0 @@
-# Haebojo_project_somin
-
-[프로젝트] 해보조_스마트선반 진행사항 요약
-https://lodissign.tistory.com/55
-
-
